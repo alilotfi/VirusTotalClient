21a6d2dcd05307238b97a1579aa4c8bad71e99c2
Ali Lotfi <alilotfi3@gmail.com>
2016-10-06 20:15:32 +0330

    Complete AppListPresenter tests

--- app/src/test/java/ir/alilo/virustotalclient/features/applist/AppListPresenterTest.kt
+++ app/src/test/java/ir/alilo/virustotalclient/features/applist/AppListPresenterTest.kt
@@ -1,11 +1,14 @@
 package ir.alilo.virustotalclient.features.applist
 
 import android.app.Application
+import ir.alilo.virustotalclient.datasources.db.App
 import ir.alilo.virustotalclient.injection.AndroidModule
 import ir.alilo.virustotalclient.injection.DaggerApplicationComponent
+import ir.alilo.virustotalclient.utils.callbackWithCode
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Matchers
+import org.mockito.Matchers.eq
 import org.mockito.Mockito
 import org.mockito.Mockito.`when` as wen
 
@@ -30,22 +33,66 @@ class AppListPresenterTest {
 
     @Test
     fun loadSystemApps() {
-        wen(interactor.fetchApps(true, 0))
-                .then { presenter.onAppsRetrieved(mutableListOf(), 0) }
-        wen(interactor.fetchApps(true, 1))
-                .then { presenter.onAppsRetrieved(mutableListOf(), 1) }
+        val appsList: List<App> = mutableListOf()
+
+        wen(interactor.fetchApps(eq(true), Matchers.anyInt()))
+                .callbackWithCode { presenter.onAppsRetrieved(appsList, it) }
 
         presenter.loadApps(true)
 
+        checkLoadAppsResult(appsList, true)
+    }
+
+    @Test
+    fun loadRegularApps() {
+        val appsList: List<App> = mutableListOf()
+
+        wen(interactor.fetchApps(eq(false), Matchers.anyInt()))
+                .callbackWithCode { presenter.onAppsRetrieved(appsList, it) }
+
+        presenter.loadApps(false)
+
+        checkLoadAppsResult(appsList, false)
+    }
+
+    private fun checkLoadAppsResult(appsList: List<App>, system: Boolean) {
         val order = Mockito.inOrder(view, interactor)
         order.verify(view).showLoading()
-        order.verify(interactor).fetchApps(Matchers.eq(true), Matchers.anyInt())
-        order.verify(view).showApps(mutableListOf())
+        order.verify(interactor).fetchApps(eq(system), Matchers.anyInt())
+        order.verify(view).showApps(appsList)
         order.verify(view).hideLoading()
     }
 
     @Test
-    fun reloadApps() {
+    fun reloadSystemApps() {
+        val appsList: List<App> = mutableListOf()
 
+        wen(interactor.fetchApps(eq(true), Matchers.anyInt()))
+                .callbackWithCode { presenter.onAppsRetrieved(appsList, it) }
+
+        presenter.reloadApps(true)
+
+        checkReloadAppsResult(appsList, true)
+    }
+
+    @Test
+    fun reloadRegularApps() {
+        val appsList: List<App> = mutableListOf()
+
+        wen(interactor.fetchApps(eq(false), Matchers.anyInt()))
+                .callbackWithCode { presenter.onAppsRetrieved(appsList, it) }
+
+        presenter.reloadApps(false)
+
+        checkReloadAppsResult(appsList, false)
+    }
+
+    private fun checkReloadAppsResult(appsList: List<App>, system: Boolean) {
+        val order = Mockito.inOrder(view, interactor)
+        order.verify(view).showLoading()
+        order.verify(view).clearApps()
+        order.verify(interactor).fetchApps(eq(system), Matchers.anyInt())
+        order.verify(view).showApps(appsList)
+        order.verify(view).hideLoading()
     }
 }
